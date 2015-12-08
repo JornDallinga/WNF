9d84372fcecc6315a2bb9d4e16ac118043e120dc
JornDallinga <jorndallinga@gmail.com>
2015-12-08 01:50:31 +0100

    download from FTP

--- Main.R
+++ Main.R
@@ -1,1 +1,10 @@
-testing
+# Installing packages
+if (!require(raster)) install.packages('raster')
+if (!require(RCurl)) install.packages('RCurl')
+
+# Loading packages
+library(raster)
+library(RCurl)
+
+Download_WWF(data = 'Forest Cover', year = 2000, mydir = 'data')
+Download_WWF(data = 'MODIS Mosaic.overviews', year = 'MODIS2005.Overviews', mydir = 'data/MODIS_MOSAIC')
---
+++ R/Download_FTP.R
@@ -0,0 +1,35 @@
+data <- 'Forest Cover'
+year <- 2000
+mydir <- 'data'
+
+Download_WWF <- function(data,year,mydir){
+  
+  # get login information
+  baseurl <- sprintf(("ftp://globil.panda.org/%s/%s/"), data, year)
+  un = readline("Type the username:")
+  pw = readline("Type the password:")
+  upw = paste(un, pw, sep = ":")
+  
+  # list files on FTP server
+  filenames <- getURL(baseurl, userpwd = upw,
+                      ftp.use.epsv = FALSE, dirlistonly = TRUE)
+  filenames <- strsplit(filenames, "\r\n")
+  filenames <- unlist(filenames)
+  
+  urls <- grep(pattern = ".tif$", filenames, value = T)
+  print(urls)
+  
+  #Location urls = readline("Which file?:")
+  
+  # generate the full URLS
+  fileurls = paste0(baseurl, urls)
+  # generate the destination file paths
+  filepaths = file.path(mydir, urls)
+  # create a container for loop outputs
+  res = vector("list", length = length(filepaths))
+  # loop through urls and download
+  for(i in seq(length(fileurls)))
+    writeBin(getBinaryURL(fileurls[i], userpwd = upw), 
+             con = filepaths[i])  
+  
+}
