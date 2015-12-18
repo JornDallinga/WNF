c85a252ece8369b0c73db892d83ba8372fb2ea19
Your Namejorndallinga <jorndallinga@gmail.com>
2015-12-18 14:34:00 +0100

    added regions to th extent function

--- R/Extent.R
+++ R/Extent.R
@@ -1,38 +1,31 @@
-# list all files in folder
-list_file <- list.files(sprintf('%s/',mydir), full.names=T)
-x <- raster(list_file[1])
-x <- setMinMax(x)
+Extent_func <- function(year = year, mydir = mydir, country = country, region = region){
 
-## temp script for bounding box (extent)
-output <- drawExtent(show=TRUE, col="red")
-test <- spTransform(output, CRS = '+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0')
-test_crop <- crop(x, test)
-reproject <- projectRaster(from = test_crop, crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0", method = 'ngb')
-
-BB <- spTransform(output, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
-
-## end temp script for bounding box (extent)
-
-
-
-if (is.null(country) == TRUE){
-  project_extent <- extent(output)
-  ## Create the clipping polygon
-  BB <- as(extent(project_extent@xmin, project_extent@xmax, project_extent@ymin, project_extent@ymax), "SpatialPolygons")
-  proj4string(BB) = CRS(as.character(x@crs))
-  BB <- spTransform(BB, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-  output <- BB
-} else if (is.null(country) == F) {
-  CountryShape <- getData('GADM', country = as.character(country), level=1)
-  coordsys <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
-  spTransform(CountryShape, coordsys)
-  output <- CountryShape
+  ## end temp script for bounding box (extent)
+  
+  if (is.null(country) == TRUE){
+    project_extent <- extent(x)
+    
+    ## Create the clipping polygon
+    BB <- as(extent(project_extent@xmin, project_extent@xmax, project_extent@ymin, project_extent@ymax), "SpatialPolygons")
+    proj4string(BB) = CRS(as.character(x@crs))
+    BB <- spTransform(BB, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+    output <- BB
+    
+  } else if (is.null(region) == F) {
+    CountryShape <- getData('GADM', country = as.character(country), level=1)
+    coordsys <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
+    spTransform(CountryShape, coordsys)
+    output <- subset(CountryShape, CountryShape@data$NAME_1 == region)
+    
+        
+  } else if (is.null(region) == T) {
+    CountryShape <- getData('GADM', country = as.character(country), level=1)
+    coordsys <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
+    spTransform(CountryShape, coordsys)
+    output <- CountryShape
+    
+  } else {
+    print("No valid name")
+  }
+  return(output)
 }
-
-
-
-Raster <- rast.list[[t]]
-plot_Raster <- spTransform(buffer, CRS(proj4string(Raster))) 
-plot_crop <- crop(Raster, plot_Raster)
-reproject <- projectRaster(from = test_crop, crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0", method = 'ngb')
-new_list[t] <- reproject
