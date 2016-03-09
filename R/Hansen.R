1141ac3c942697ea57d1641cb4b41bcc8bc22e12
JornDallinga <jorndallinga@gmail.com>
2016-03-09 01:05:12 +0100

    Adjusted function to download and analyse hansen data

--- R/Hansen.R
+++ R/Hansen.R
@@ -1,6 +1,3 @@
-
-#-------------------------------------- Function -----------------------------------
-
 Hansen <- function(threshold = threshold, year = year, output = output){
   ## Create variable Area Of Interest (aio)
   #aoi <- readRDS(file = 'data/BufferWGS.rds', refhook = NULL)
@@ -12,77 +9,32 @@ Hansen <- function(threshold = threshold, year = year, output = output){
   ## Download GFC data
   download_tiles(tiles, 'data/Hansen', data_year = 2015)
   
-  #set cores
-  beginCluster( detectCores()-1 )
-  
   ## Extract data from tiles
-  system.time(extract_gfc(aoi, data_folder = "data/Hansen", filename="data/extract_hansen/GFC_extract.tif", overwrite=TRUE, stack = 'change', data_year = 2015))
+  Ex_data <- extract_gfc(aoi, data_folder = "data/Hansen", stack = 'change', data_year = 2015)
 
   
   ## Apply threshold to extracted data 
-  system.time(threshold_gfc(stack("data/extract_hansen/GFC_extract.tif"), forest_threshold = threshold, 
-                         filename="data/extract_hansen/GFC_extract_thresholded.tif", overwrite=TRUE, progress = 'text'))
+  thres <- threshold_gfc(Ex_data, forest_threshold = threshold)
   
-  
-  
-  ## Masking for water perc calculations
-  #mask_water <- mask(stack('data/extract_hansen/GFC_extract_thresholded.tif'), aoi)
-  
-  
-  ## Masking gfc data to aoi
-  system.time(mask(stack('data/extract_hansen/GFC_extract_thresholded.tif'), aoi, filename = 'data/gfc_threshold/Hansen_mask.tif', overwrite = T, progress = 'text'))
 
   ## anual stack of years
-  system.time(annual_Hansen <- annual_stack(stack('data/extract_hansen/GFC_extract_thresholded.tif'), data_year = 2015))
-  
-  #stop snowfall
-  endCluster()
-  
-  # retrieve water
-  Water <- freq(mask_water$datamask, digits= 0, value = 2, useNA = no)
-  listvalues <- values(mask_water$datamask)
-  countcells <- count(listvalues)
-  countcells <- countcells[!is.na(countcells$x),]
-  total_cells <- sum(countcells$freq)
-  ## percentage water
-  Water_perc <- (Water / total_cells) * 100
+  annual_Hansen <- annual_stack(stack(thres), data_year = 2015)
   
   # Select a year from the annual Hansen dataset
   select_y <- sprintf("y%s",year)
-  print("Loading1")
   subset_Year <- subset(annual_Hansen, subset = select_y)
-  print("Loading2")
+
   
   # Create binary forest cover map and figure output
   
   if (year == 2000){
     ## create forest cover mask year 2000
-    Figure_output <- mask_gfc
-    
-    Figure_output$forest2000[Figure_output$forest2000 == 1] <- 2 # Forest
-    Figure_output$forest2000[Figure_output$forest2000 == 0] <- 1 # Non-Forest
-    Figure_output$datamask[Figure_output$datamask == 2] <- 3 # water
-    Figure_output$datamask[Figure_output$datamask == 0] <- 4 # No data
-    suppressWarnings(Figure_output$datamask[Figure_output$datamask < 3] <- NA) # Nodata for merging
-    Figure_output <- merge(Figure_output$datamask, Figure_output$forest2000, overlap = T)
-    names(Figure_output) <- "Hansen"
-    
+
     mask_gfc <- mask_gfc$forest2000
     
   } else if (year >= 2001 & year <= 2014){
     ## create forest cover mask of years 2001 till 2014
-    Figure_output <- subset_Year
-    
-    Figure_output[Figure_output == 2] <- 8 # Non-forest
-    Figure_output[Figure_output == 1] <- 2 # Forest
-    Figure_output[Figure_output == 8] <- 1 # Non-forest
-    
-    Figure_output[Figure_output == 3] <- 1 # Non-forest
-    Figure_output[Figure_output == 4 ] <- 2 # Forest gain to Forest
-    Figure_output[Figure_output == 5 ] <- 2 # Forest loss and gain to Forest
-    Figure_output[Figure_output == 6] <- 3 # Water
-    names(Figure_output) <- "Hansen"
-    
+
     subset_Year[subset_Year == 2 ] <- 0
     subset_Year[subset_Year == 3 ] <- 0
     subset_Year[subset_Year == 4 ] <- 1
@@ -95,7 +47,8 @@ Hansen <- function(threshold = threshold, year = year, output = output){
   
   names(mask_gfc) <- "Hansen"
   
-  return_list <- list(mask_gfc, Water_perc, Figure_output)
-  
-  return (return_list)
+  return (mask_gfc)
 }
+
+
+# KML(mask_gfc, file = 'data/output/test.kml') 
