4531f3e3e276aada95875d3366568991971cbe0f
JornDallinga <jorndallinga@gmail.com>
2016-03-09 01:04:33 +0100

    function update

--- R/FCC.R
+++ R/FCC.R
@@ -1,21 +1,27 @@
 list.files('data/')
 
-WWF2005 <- raster('data/borneo_2005_forest.tif')
-WWF2010 <- raster('data/borneo_2010_forest.tif')
+WWF2000 <- raster('data/cerrado_forest_2000.tif')
+WWF2010 <- raster('data/cerrado_af_2010_forest.tif')
 
-WWF2005 <- setMinMax(WWF2005)
+WWF2000 <- setMinMax(WWF2000)
 WWF2010 <- setMinMax(WWF2010)
 
 WWF2010t <- WWF2010
 
 
-WWF2010t[WWF2010 == 1 & WWF2005 == 1] <- 2 # Forest
-WWF2010t[WWF2010 == 0 & WWF2005 == 1] <- 3 # Loss
-WWF2010t[WWF2010 == 1 & WWF2005 == 0] <- 4 # Gain
-WWF2010t[WWF2010 == 0 & WWF2005 == 0] <- 5 # Non-Forest
+WWF2010t[WWF2010 == 1 & WWF2000 == 1] <- 2 # Forest
+WWF2010t[WWF2010 == 0 & WWF2000 == 1] <- 3 # Loss
+WWF2010t[WWF2010 == 1 & WWF2000 == 0] <- 4 # Gain
+WWF2010t[WWF2010 == 0 & WWF2000 == 0] <- 5 # Non-Forest
 
 
 
-writeRaster(WWF2010t, filename = 'data/output/FFC2005_2010.tif', overwrite = T)
+writeRaster(WWF2010t, filename = 'data/output/FFC2000_2010_cerrado.tif', overwrite = T)
 
 plot(WWF2000_2005)
+
+
+# write to raster for masking
+writeRaster(WWF2000, filename = 'data/output/FFC2000_mask_cerrado.tif', overwrite = T)
+writeRaster(WWF2010, filename = 'data/output/FFC2010_mask_cerrado.tif', overwrite = T)
+
