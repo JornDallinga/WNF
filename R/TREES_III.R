ea4c7b6d8c71f24e8433f45b4af5a68fe9acb06b
JornDallinga <jorndallinga@gmail.com>
2016-03-10 22:55:51 +0100

    added Forest cover and forest cover change conversions

--- R/TREES_III.R
+++ R/TREES_III.R
@@ -1,45 +1,45 @@
 # Retrieve Projection system
-raster_sat <- raster('data/TREES_III/S17_W050/S17_W050_00_c.tif')
+raster_sat <- raster('data/TREES_III/S12_W053/S13_W053_10_c.tif')
 as.character(raster_sat@crs)
 
 # assign projection system to shapefile and load into R environment
 proj <- as.character(raster_sat@crs)
-sat <- readOGR('data/TREES_III/S17_W050/S17_W050.shp', layer = 'S17_W050')
+sat <- readOGR('data/TREES_III/S12_W044/S12_W044.shp', layer = 'S12_W044')
 
 # load WWF data
 WWF2000 <- raster('data/Transformed_Rasters/cerrado_forest_2000.tif')
 WWF2010 <- raster('data/Transformed_Rasters/cerrado_2010.tif')
 
 
-# reproject sat to WWF projection
-#reproj <- as.character(WWF2000@crs)
-#sat1 <- reproject(sat, CRS = reproj)
+rast <- Hansen(threshold = threshold, year = 2010, output = buff, UTM = T)
 
-sat1 <- spTransform(x = sat, CRSobj = WWF2000@crs)
+sat1 <- spTransform(x = sat, CRSobj = WWF2010@crs)
 
 # plot data for testing
-plot(WWF2000)
+plot(WWF2010)
 plot(sat1, add = T, col = 'red')
 
-# test area
-
-reproj <- as.character(WWF2000@crs)
+# reprojecting
 
 
 WWF_2000_crop <- crop(WWF2000, sat1)
 WWF_2010_crop <- crop(WWF2010, sat1)
 
-WWF_2000 <- setMinMax(WWF_2000_crop)
+WWF_2000 <- projectRaster(from = WWF_2000_crop, to = WWF_2010_crop, method = 'ngb')
+
+WWF_2000 <- setMinMax(WWF_2000)
 WWF_2010 <- setMinMax(WWF_2010_crop)
 
-rr <- CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
 
-WWF_2010r <- reproject(WWF_2000, CRS = rr)
-WWF_2000r <- reproject(WWF_2010, CRS = rr)
+
+#rr <- CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+
+#WWF_2010r <- reproject(WWF_2000, CRS = rr)
+#WWF_2000r <- reproject(WWF_2010, CRS = rr)
 
 # Prepare WWF data for cover change
 
-WWF2010t <- WWF_2010r 
+WWF2010t <- WWF_2010 
 
 WWF2010t[WWF_2010r == 1 & WWF_2000r == 1] <- 2 # Forest
 WWF2010t[WWF_2000r == 0 & WWF_2000r == 1] <- 3 # Loss
@@ -48,25 +48,56 @@ WWF2010t[WWF_2000r == 0 & WWF_2000r == 0] <- 5 # Non-Forest
 
 plot(WWF2010t)
 
-## prepare landsat selection: raster testing
+################################################# Forest Cover  ####################################################################
 
-r <- raster(ncol=56, nrow=54)
-extent(r) <- extent(poly)
-rp <- rasterize(x = sat1, field = as.numeric(as.character(sat1$CH_00_10)),  y = WWF2000)
+## recalculate values for forest cover 
+
+rp <- rasterize(x = sat1, fun = modal, field = as.numeric(as.character(sat1$X2010)),  y = WWF_2010 )
+t <- focal(rp, w=matrix(1,3,3), fun=modal)
 
 rp2 <- rp
 
-# recalculate values
+# recalculate values for forest cover
+
+rp2[rp == 10] <- 1 # Forest
+rp2[rp != 10] <- 0 # Non-Forest
+
+## confusion matrix
+# Calculate accuracy for polygons
+tabletest <- table(rp2@data@values,WWF_2010@data@values, dnn = c('REF','WWF'))
+outcome_poly <- confusionMatrix(tabletest)
+outcome_poly
+
+
+################################################# Forest Cover Change ####################################################################
+
+# recalculate values for forest cover change
+rp <- rasterize(x = sat1, fun = modal,field = as.numeric(as.character(sat1$CH_00_10)),  y = WWF_2000)
+
+# copy dataset
+rp2 <- rp
+
+# replace values for reference raster
 rp2[rp == 1010] <- 2 # Forest
-rp2[rp == 1010] <- 3 # Loss
-rp2[rp == 1010] <- 4 # Gain
-rp2[rp == 1010] <- 5 # Non-Forest
+rp2[(stri_sub(rp2@data@values,1,2) == 10) & (stri_sub(rp2@data@values,3,4) != 10)] <- 3 # loss
+rp2[(stri_sub(rp2@data@values,3,4) == 10)] <- 4 # Gain
+rp2[rp2 != c(2,3,4)] <- 5 # Non-Forest
 
-WWF2010t[WWF_2000r == 0 & WWF_2000r == 1] <- 3 # Loss
-WWF2010t[WWF_2000r == 1 & WWF_2000r == 0] <- 4 # Gain
-WWF2010t[WWF_2000r == 0 & WWF_2000r == 0] <- 5 # Non-Forest
+# Select values for WNF dataset
+WWF2010t <- WWF_2000 
 
-# select values
-sel <- subset(rp2@data@values, substr(rp2@data@values, 2, 4) == c('12','10'))
+WWF2010t[WWF_2010 == 1 & WWF_2000 == 1] <- 2 # Forest
+WWF2010t[WWF_2010 == 0 & WWF_2000 == 1] <- 3 # Loss
+WWF2010t[WWF_2010 == 1 & WWF_2000 == 0] <- 4 # Gain
+WWF2010t[WWF_2010 == 0 & WWF_2000 == 0] <- 5 # Non-Forest
 
-as.matrix(rp2@data@values)
+# set table classes even
+pred <- WWF2010t@data@values
+act <- rp2@data@values
+u = union(pred, act)
+t = table(factor(act, u), factor(pred, u), dnn = c('Pred', 'WNF'))
+
+# create confusion matrix
+outcome_poly <- confusionMatrix(t)
+outcome_poly
+
